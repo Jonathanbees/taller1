22c7a1544d554ae5447a4acd338dbe0f0bd53624
Jonathanbees <jonathanbetancurespinosa@gmail.com>
2023-08-04 18:51:04 -0500

    formulario de búsqueda

--- sitio1/urls.py
+++ sitio1/urls.py
@@ -19,9 +19,13 @@ from django.urls import path
 from sitio1.views import saludo
 from movie import views as movieViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('', movieViews.home),
     path('about/', movieViews.about),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
